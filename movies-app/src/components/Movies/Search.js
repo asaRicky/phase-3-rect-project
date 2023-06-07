ac8f08658db9fb2584d6425728481d4bb321352a
asaRicky <derrickomondi208@gmail.com>
2023-06-07 10:31:57 +0300

    done

---
+++ movies-app/src/components/Movies/Search.js
@@ -0,0 +1,18 @@
+import React, { useState} from 'react';
+
+function Search({ onSearch }) {
+    const [query, setQuery] = useState("");
+
+    function handleQuery(e) {
+        e.preventDEfault()
+        onSearch(query)
+    }
+  return (
+    <nav>
+       <input value={query} onChange={ e => setQuery(e.target.value)}></input>
+      <button type="submit" onClick={e => handleQuery(e)}>Search</button>
+    </nav>
+  );
+}
+
+export default Search;
