beeaf870e2035959a59f0b0c5b13f916f610ad41
asaRicky <derrickomondi208@gmail.com>
2023-06-07 09:34:58 +0300

    done

--- movies-app/src/components/Movies/Allmovies.js
+++ movies-app/src/components/Movies/Allmovies.js
@@ -1,35 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Allmovies({ movies, handleDeleteMovie }) {
+  function handleDeleteClick(id) {
+    fetch(`https://data-j87g.onrender.com/movies/destroy/${id}`, {
+      method: "DELETE",
+    });
 
-    function handleDeleteClick(id) {
-        fetch ('https://data-j87g.onrender.com/movies/destroy/${id}', {
-          method: "DELETE",
-        })
-    }handleDeleteMovie(id)
+    handleDeleteMovie(id);
+  }
+
   return (
-    <div className='cards'>
-      {movies.map((movie) =>
-        <div key={movie.id} className='card'>
+    <div className="cards">
+      {movies.map((movie) => (
+        <div key={movie.id} className="card">
           <h1>{movie.title}</h1>
           <p>{movie.description}</p>
-          <img className='card-img-top' src={movie.movie_url}></img>
+          <img
+            className="card-img-top"
+            src={movie.movie_url}
+            alt={movie.movie_url}
+          ></img>
           <h2>{movie.year}</h2>
           {movie.originally_fetched === false && (
             <button onClick={() => handleDeleteClick}>
               <span role="img" aria-label="delete">
-              🗑
+                🗑
               </span>
             </button>
           )}
           {movie.originally_fetched === false && (
-            <Link to={`/movies/${movie.id}`}>Update</Link>
+            <Link to={`/update/${movie.id}`}>Update</Link>
           )}
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
 export default Allmovies;
--- movies-app/src/components/Movies/Dashboard.js
+++ movies-app/src/components/Movies/Dashboard.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Search from './search';
-import Allmovies from './Allmovies';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Search from "./Search";
+import Allmovies from "./Allmovies";
 
 function Dashboard() {
 
-    const navigate = useNavigate()
-    const [movies, setMovies] = useState([])
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState([]);
 
-    function handleDeleteMovie(id) {
-        const updateMovies = movies.filter((movie) => movie.id !== id);
-        setMovies(updateMovies);
-    }
-    useEffect(() => {
-        fetch('https://ldata-j87g.onreader.com/movies')
-        .then((response) => response.json())
-        .then((data) => setMovies(data))
-    }, [])
+  function handleDeleteMovie(id) {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies);
+  }
 
-    function handleSearch(pquery) {
-        fetch ('https://data-j87g.onrender.com/search?query=${query}')
-        .then((response) => response.json())
-        .then((result) => {
-            setMovies(results)
-            navigate("/dashboard")
-        })
-    }
+  useEffect(() => {
+    fetch("https://data-j87g.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
 
+  function handleSearch(query) {
+    fetch(`https://data-j87g.onrender.com/search?query=${query}`)
+      .then((response) => response.json())
+      .then((results) => {
+        setMovies(results);
+        navigate("/dashboard");
+      });
+  }
 
   return (
     <div>
-      <nav className='dashnav'>
-        <Link to="./addmovie">Add Movie</Link>
-        <Search onsearch={handleSearch}/>
+      <nav className="dashnav">
+        <Link to="/addmovie">Add Movie</Link>
+        <Search onSearch={handleSearch} />
       </nav>
-      <Allmovies movies={movies} handleDeleteMovies={handleDeleteMovie}/>
+      <Allmovies movies={movies} handleDeleteMovie={handleDeleteMovie} />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
